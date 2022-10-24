0322cbc832331d06627b9fac7fcdcfc564aa798b
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-24 18:17:43 +0500

    User controller and user service done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use App\Http\Services\UserService;
+
+class UserController extends Controller
+{
+    protected $service;
+
+    public function __construct()
+    {
+        $this->service = new UserService();
+    }
+
+    public function index()
+    {
+        $users = $this->service->index();
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function show($id)
+    {
+        $user = $this->service->show($id);
+        return view('admin.user.show', compact('user'));
+    }
+
+    public function create()
+    {
+        $user = $this->service->create();
+        return view('admin.users.create', compact('user'));
+    }
+
+    public function store(UserRequest $request)
+    {
+        $this->service->store($request->validated());
+        return redirect()->route('admin.users.index')->with('success', 'New User added!');
+    }
+
+    public function edit($id)
+    {
+        $user = $this->service->show($id);
+        return view('admin.user.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, $id)
+    {
+        $this->service->update($request->validated(), $id);
+        return redirect()->route('admin.users.index')->with('success', 'User updated!');
+    }
+
+    public function destroy($id)
+    {
+        $this->service->destroy($id);
+        return redirect()->route('admin.user.destroy')->with('success', 'User deleted!');
+    }
+}
---
+++ app/Http/Services/UserService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\User;
+
+class UserService
+{
+    public function index()
+    {
+        return User::where('id', auth()->user()->id)->orderByDesc('created_at')->get();
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function create()
+    {
+        return new User();
+    }
+
+    public function store(array $data)
+    {
+        User::create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $user = $this->show($id);
+        $user->update($data);
+    }
+
+    public function destroy($id)
+    {
+        $user = $this->show($id);
+        $user->delete();
+    }
+}
