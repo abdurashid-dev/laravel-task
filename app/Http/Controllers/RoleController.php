89bfb2f94c0772110d5daa4ee2a2e73daa6ddc6a
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-24 09:06:45 +0500

    role service added

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct()
+    {
+        $this->service =
+        $this->middleware('permission:roles-list')->only('index');
+    }
+
+    public function index()
+    {
+
+    }
 }
