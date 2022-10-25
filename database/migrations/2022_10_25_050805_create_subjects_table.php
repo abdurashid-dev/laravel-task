245b4864b739af2b047b34d7f1a1906eb4205c5d
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-25 11:00:44 +0500

    teacher id removed from subjects migrations

--- database/migrations/2022_10_25_050805_create_subjects_table.php
+++ database/migrations/2022_10_25_050805_create_subjects_table.php
@@ -15,7 +15,6 @@ return new class extends Migration {
         Schema::create('subjects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('teacher_id');
             $table->timestamps();
         });
     }
