1e989a44bf865eec402981ff36290aac482f9a77
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-25 10:56:56 +0500

    teachers form bugs fixed

--- resources/views/admin/teachers/form.blade.php
+++ resources/views/admin/teachers/form.blade.php
@@ -1,6 +1,9 @@
 <div class="mb-3">
     <label for="nameInput">Teacher name</label>
     <input type="text" id="nameInput" name="name" class="form-control" value="{{old('name') ?? $teacher->name}}">
+    @error('name')
+    <span class="text-danger">{{$message}}</span>
+    @enderror
 </div>
 <div class="mb-3">
     <label for="subjectsSelect">Choose Subject</label>
@@ -9,8 +12,11 @@
             <option value="{{$subject->id}}"
                     @if($subject_id == $teacher->$subject_id) selected @endif>{{$subject->name}}</option>
         @empty
-            <option disabled>No subjects found :(</option>
+            <option value="">No subjects found :(</option>
         @endforelse
     </select>
+    @error('subject_id')
+    <span class="text-danger">{{$message}}</span>
+    @enderror
 </div>
 <button class="btn btn-primary float-right">Save</button>
