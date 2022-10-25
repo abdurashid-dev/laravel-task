4e1d100983924e3d5c9630a3d0ccdf70ce520d33
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-25 10:34:49 +0500

    teachers create blade done

--- resources/views/admin/teachers/create.blade.php
+++ resources/views/admin/teachers/create.blade.php
@@ -3,9 +3,29 @@
     Teachers
 @endsection
 @section('content')
-    <form action="{{route('admin.teachers.store')}}" method="POST">
-        @csrf
-        <input type="text" name="name">
-        <input type="submit">
-    </form>
+    <x-headers title="Create teacher" icon="fas fa-plus" parent="Teacher" parent-route="admin.teachers.index"
+               parent-icon="fas fa-users"/>
+    <div class="card">
+        <div class="card-header"></div>
+        <div class="card-body">
+            <form action="{{route('admin.teachers.store')}}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="nameInput">Teacher name</label>
+                    <input type="text" id="nameInput" name="name" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="subjectsSelect">Choose Subject</label>
+                    <select name="subject_id" id="subjectsSelect" class="form-control">
+                        @forelse($subjects as $subject)
+                            <option value="{{$subject->id}}">{{$subject->name}}</option>
+                        @empty
+                            <option disabled>No subjects found :(</option>
+                        @endforelse
+                    </select>
+                </div>
+                <button class="btn btn-primary float-right">Save</button>
+            </form>
+        </div>
+    </div>
 @endsection
