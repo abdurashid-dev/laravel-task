47906fdac45412adc445de07e94079adb32c085b
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-25 11:42:15 +0500

    lessons done

--- resources/views/admin/lessons/edit.blade.php
+++ resources/views/admin/lessons/edit.blade.php
@@ -1,17 +1,17 @@
 @extends('admin.layouts.app')
 @section('title')
-    Subjects
+    Lessons
 @endsection
 @section('content')
-    <x-headers title="Edit subject" icon="fas fa-plus" parent="Subjects" parent-route="admin.subjects.index"
+    <x-headers title="Edit lesson" icon="fas fa-plus" parent="Lessons" parent-route="admin.lessons.index"
                parent-icon="fas fa-book-reader"/>
     <div class="card">
         <div class="card-header"></div>
         <div class="card-body">
-            <form action="{{route('admin.subjects.update', $subject)}}" method="POST">
+            <form action="{{route('admin.lessons.update', $lesson)}}" method="POST">
                 @csrf
                 @method('PUT')
-                @include('admin.subjects.form')
+                @include('admin.lessons.form')
             </form>
         </div>
     </div>
