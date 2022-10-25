4d81c22d8d99e5622aefc7a1635ed56142b99d4a
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-25 11:39:21 +0500

    lessons show done

--- resources/views/admin/lessons/show.blade.php
+++ resources/views/admin/lessons/show.blade.php
@@ -1,21 +1,21 @@
 @extends('admin.layouts.app')
 @section('title')
-    Subject
+    Lesson
 @endsection
 @section('content')
-    <x-headers title="Subject({{$subject->name}})" icon="fas fa-eye" parent="Subjects"
-               parent-route="admin.subjects.index" parent-icon="fas fa-book-reader"/>
+    <x-headers title="Lesson({{$lesson->name}})" icon="fas fa-eye" parent="Lessons"
+               parent-route="admin.lessons.index" parent-icon="fas fa-book-reader"/>
     <div class="card">
         <div class="card-header"></div>
         <div class="card-body table-responsive">
             <table class="table table-bordered">
                 <tr>
                     <th>ID</th>
-                    <td>{{$subject->id}}</td>
+                    <td>{{$lesson->id}}</td>
                 </tr>
                 <tr>
                     <th>Name</th>
-                    <td>{{$subject->name}}</td>
+                    <td>{{$lesson->name}}</td>
                 </tr>
             </table>
         </div>
