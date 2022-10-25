c88bef19c4b3000617198d9234f7bf319a0c975a
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-25 11:23:12 +0500

    subject edit done

--- resources/views/admin/subjects/edit.blade.php
+++ resources/views/admin/subjects/edit.blade.php
@@ -3,13 +3,14 @@
     Subjects
 @endsection
 @section('content')
-    <x-headers title="Create subject" icon="fas fa-plus" parent="Subjects" parent-route="admin.subjects.index"
+    <x-headers title="Edit subject" icon="fas fa-plus" parent="Subjects" parent-route="admin.subjects.index"
                parent-icon="fas fa-users"/>
     <div class="card">
         <div class="card-header"></div>
         <div class="card-body">
-            <form action="{{route('admin.subjects.store')}}" method="POST">
+            <form action="{{route('admin.subjects.update', $subject)}}" method="POST">
                 @csrf
+                @method('PUT')
                 @include('admin.subjects.form')
             </form>
         </div>
